e81a4b1f6e0f50a2d01a50add51417105636f1b0
Mathijs-D85 <mduisdecker@gmail.com>
2025-11-27 15:53:42 +0100

    Security: Moved API keys to .env

--- config.ts
+++ config.ts
@@ -1,17 +1,17 @@
 
-export const SPOTIFY_CLIENT_ID = '9af64d743553409a8dee1af73dd4f51b';
+export const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 
 // Dynamisch de redirect URI bepalen op basis van waar de app draait
 // Zorg dat deze URI exact overeenkomt in je Spotify Dashboard (bijv. https://jouw-app.vercel.app/callback)
 export const REDIRECT_URI = `${window.location.origin}/callback`;
 
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDPy23V63k6xubpyYPwkPpMHuMIGPKUQdo",
-  authDomain: "spotify-spel.firebaseapp.com",
-  databaseURL: "https://spotify-spel-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "spotify-spel",
-  storageBucket: "spotify-spel.firebasestorage.app",
-  messagingSenderId: "22024851172",
-  appId: "1:22024851172:web:1b00b3d8a55727357126f1",
-  measurementId: "G-2SGV6ZJWHM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
